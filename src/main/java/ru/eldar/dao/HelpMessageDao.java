02ad453579b66a829cb2ec379f4d0bdd092a6a3d
Eldar Gainanov <eldar.gaynanov@simbirsoft.com>
2024-02-03 19:35:34 +0500

    add java docs

--- src/main/java/ru/eldar/dao/HelpMessageDao.java
+++ src/main/java/ru/eldar/dao/HelpMessageDao.java
@@ -2,9 +2,22 @@ package ru.eldar.dao;
 
 import java.util.List;
 
+/**
+ * The interface for interacting with message storage/
+ */
 public interface HelpMessageDao {
 
+    /**
+     * This method gets all messages from storage
+     *
+     * @return the all messages
+     */
     List<String> getAll();
 
+    /**
+     * Add message to storage
+     *
+     * @param message the new message
+     */
     void addMessage(String message);
 }
